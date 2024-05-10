a4c282ecff2b40fd45519ff53045a35aa1c4261f
rkofman <35267+rkofman@users.noreply.github.com>
2024-05-10 14:22:20 -0700

    experimenting with onscan.js

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,9 +1,15 @@
 import('./assets/main.css')
+import onScan from 'onscan.js'
 
 import App from './App.svelte'
 
 const app = new App({
   target: document.getElementById('app') as HTMLElement
+})
+
+onScan.attachTo(document)
+document.addEventListener('scan', function (sScancode: string, iQuantity: string) {
+  alert(iQuantity + 'x ' + sScancode)
 })
 
 export default app
