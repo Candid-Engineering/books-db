f4e43dd5f389689951ef3388139bda0a6c62a41c
Jennifer Martin <jenomega@gmail.com>
2024-10-17 10:39:50 -0700

    A cleaner way of doing the loading/error message

--- src-ui/lib/components/BooksTable.svelte
+++ src-ui/lib/components/BooksTable.svelte
@@ -15,25 +15,13 @@
     hasRead: true,
   }
 
-  let loading_visibility = "hidden";
-  let loading_text = "";
 
   if (books.value.length == 0) {
     books.add(initialBook)
   }
 
   const addBook = async (isbn: string): Promise<void> => {
-    try {
-      loading_visibility = "visible"
-      loading_text = "Loading..."
       books.add(await getByISBN(isbn))
-      loading_visibility = "hidden"
-      loading_text = "";
-
-    } catch (error) {
-      loading_text = "Error Fetching Book!"
-      loading_visibility = "visible"
-    }
   }
 
   type scanEvent = {
@@ -44,7 +32,6 @@
   }
   let promise: Promise<void> | undefined;
   const handleScan = async (event: scanEvent): Promise<void> => {
-
     promise = addBook(event.detail.scanCode)
   }
   const handleEdit = (book: Book, field: keyof Book, e: Event) => {
@@ -72,10 +59,17 @@ const listenForBarcodes: Action<HTMLElement, undefined, ScanAttributes> = (node:
   let isLoading = false
 </script>
 
+
 <button on:click={() => onScan.simulate(document, '1234567890123')}>
 	Simulate ISBN
 </button>
-<span id="scan_progress" style="visibility: {loading_visibility};">{loading_text}</span>
+
+{#await promise}
+  <p>Loading...</p>
+{:catch error}
+  <p style="color: red">{error.message}</p>
+{/await}
+
 <svelte:document use:listenForBarcodes on:scan={handleScan} />
 <table class="table is-fullwidth">
   <thead>
