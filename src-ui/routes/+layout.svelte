ae2ca45eb00442068c2af2b11613455e22c3cbf9
rkofman <35267+rkofman@users.noreply.github.com>
2024-11-01 00:42:33 -0700

    Add some basic CSS layout and visual classes from Bulma.

--- src-ui/routes/+layout.svelte
+++ src-ui/routes/+layout.svelte
@@ -1,6 +1,41 @@
-<nav>
-  <a href="/">Home</a>
-  <a href="/about">About</a>
-</nav>
+<script lang="ts">
+  import 'bulma/css/bulma.css'
+</script>
 
-<slot></slot>
+<svelte:head>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+</svelte:head>
+
+<header>
+  <nav class="navbar section pt-0" aria-label="main navigation">
+    <!-- See: https://bulma.io/documentation/components/navbar/ -->
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">📚 BooksDB </a>
+      <a
+        href="#placeholder"
+        role="button"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <!-- "hamburger menu" lines: -->
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item" href="/">Home</a>
+        <a class="navbar-item" href="/about">About</a>
+      </div>
+    </div>
+  </nav>
+</header>
+<main>
+  <section class="section">
+    <slot></slot>
+  </section>
+</main>
