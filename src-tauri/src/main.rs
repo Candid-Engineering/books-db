2ec39196f4fd69c5086142476540762febf4df0c
rkofman <35267+rkofman@users.noreply.github.com>
2024-11-09 12:28:44 -0800

    removed references to migrations in rust

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,6 @@
 
 use std::env;
 
-use migrations::lib::get_migrations;
-
-pub mod migrations;
 pub mod plugins;
 
 fn main() {
@@ -19,10 +16,7 @@ fn main() {
         // TODO(rkofman): the name of the DB should be updated based on `.env` variables.
         // `books.db` for prod, `books-dev.db` for dev, and probably `:memory:` for test.
         // Also: consider moving this to the config file (assuming per-env configs).
-        .plugin(plugins::sqlite_proxy::init(
-            "books-dev.db",
-            &get_migrations(),
-        ))
+        .plugin(plugins::sqlite_proxy::init("books-dev.db"))
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
