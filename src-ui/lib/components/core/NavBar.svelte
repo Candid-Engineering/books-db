42cebde63eb1d53fe389531da78d983abeb9a6f5
Roman Kofman <35267+rkofman@users.noreply.github.com>
2024-11-12 15:37:14 -0800

    Nav menu Bulma component (#32)
    Implements a basic version of the Bulma navbar / menu. The existing code had it implemented as static HTML (already using Bulma styles). Clicking on the "hamburger" menu didn't do anything. The new code does a few things:
    
    Makes the "hamburger" menu actually clickable (on small screens)
    Extracts the "NavBar" into its own components that takes children for rendering the included links.

---
+++ src-ui/lib/components/core/NavBar.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte'
+
+  interface Props {
+    children: Snippet | undefined
+  }
+  // TODO(rkofman): this _shouldn't_ be necessary if we have the right eslint/svelte rules setup; but alas.
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  let { children }: Props = $props()
+  let isOpen = $state(false)
+  function handleHamburgerClick() {
+    isOpen = !isOpen
+  }
+</script>
+
+<nav class="navbar section pt-4" aria-label="main navigation">
+  <!-- See: https://bulma.io/documentation/components/navbar/ -->
+  <div class="navbar-brand">
+    <a class="navbar-item" href="/">📚 BooksDB </a>
+    <a
+      onclick={handleHamburgerClick}
+      href="#placeholder"
+      role="button"
+      class="navbar-burger"
+      class:is-active={isOpen}
+      aria-label="menu"
+      aria-expanded="false"
+      data-target="navbarBasicExample"
+    >
+      <!-- "hamburger menu" icon: -->
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <!-- end "hamburger menu" icon: -->
+    </a>
+  </div>
+  <div class="navbar-menu" class:is-active={isOpen}>
+    <div class="navbar-start">
+      {@render children?.()}
+    </div>
+  </div>
+</nav>
