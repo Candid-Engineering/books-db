2be30a234822e2a7dce31471f820a6ed541a25b6
rkofman <35267+rkofman@users.noreply.github.com>
2024-11-11 22:56:21 -0800

    added onclick behavior to hamburger nav menu icon

--- src-ui/lib/components/core/Nav.svelte
+++ src-ui/lib/components/core/Nav.svelte
@@ -6,6 +6,10 @@
   }
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   let { children }: Props = $props()
+  let open = $state(false)
+  function handleHamburgerClick() {
+    open = !open
+  }
 </script>
 
 <nav class="navbar section pt-4" aria-label="main navigation">
@@ -13,22 +17,23 @@
   <div class="navbar-brand">
     <a class="navbar-item" href="/">📚 BooksDB </a>
     <a
+      onclick={handleHamburgerClick}
       href="#placeholder"
       role="button"
-      class="navbar-burger"
+      class="navbar-burger {open ? 'is-active' : ''}"
       aria-label="menu"
       aria-expanded="false"
       data-target="navbarBasicExample"
     >
-      <!-- "hamburger menu" lines: -->
+      <!-- "hamburger menu" icon: -->
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
-      <!-- end "hamburger menu" lines: -->
+      <!-- end "hamburger menu" icon: -->
     </a>
   </div>
-  <div class="navbar-menu">
+  <div class="navbar-menu {open ? 'is-active' : ''}">
     <div class="navbar-start">
       {@render children?.()}
     </div>
