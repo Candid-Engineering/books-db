5ea10c2c9512472e61783237ceb8beb992b51c94
SilverMaiden <h.specials@gmail.com>
2024-11-11 18:34:50 -0800

    Add handleEvent method that checks whether the event is blur or keydown with the Enter button, handles accordingly (saves instead of inputting new line)

--- src-ui/lib/components/BooksTableRow.svelte
+++ src-ui/lib/components/BooksTableRow.svelte
@@ -3,17 +3,33 @@
 
   export let book: Book
   export let handleEdit: (book: Book, field: keyof Book, event: Event) => void
+
+  const handleEnter = () => (event: Event) => {
+    if (event instanceof KeyboardEvent && event.key === 'Enter') {
+      event.preventDefault?.()
+      ;(event.currentTarget as HTMLTableCellElement).blur?.()
+    }
+  }
 </script>
 
 <tr>
-  <td contenteditable="true" on:blur={(e) => handleEdit(book, 'isbn10', e)}
-    >{book.isbn10 ?? book.isbn13}</td
+  <td
+    contenteditable="true"
+    on:blur={(e) => handleEdit(book, 'isbn10', e)}
+    on:keydown={handleEnter()}>{book.isbn10 ?? book.isbn13}</td
   >
-  <td contenteditable="true" on:blur={(e) => handleEdit(book, 'title', e)}>{book.title || ''} </td>
-  <td contenteditable="true" on:blur={(e) => handleEdit(book, 'authors', e)}
-    >{book.authors?.join(', ') || ''}</td
+  <td
+    contenteditable="true"
+    on:blur={(e) => handleEdit(book, 'title', e)}
+    on:keydown={handleEnter()}
+    >{book?.title || ''}
+  </td>
+  <td
+    contenteditable="true"
+    on:blur={(e) => handleEdit(book, 'authors', e)}
+    on:keydown={handleEnter()}>{book.authors?.join(', ') || ''}</td
   >
-  <td contenteditable="true" on:blur={(e) => handleEdit(book, 'tags', e)}
+  <td contenteditable="true" on:blur={(e) => handleEdit(book, 'tags', e)} on:keydown={handleEnter()}
     >{book.tags?.join(', ') || ''}</td
   >
   <td>
