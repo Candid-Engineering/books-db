5b9c8c1628a90babcb7fe2def0411a8831d5099e
Roman Kofman <35267+rkofman@users.noreply.github.com>
2024-11-15 11:16:14 -0800

    Fix storybook configuration (#39)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/sveltekit'
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|ts|svelte)'],
+  stories: ['../src-ui/**/*.mdx', '../src-ui/**/*.stories.@(js|ts|svelte)'],
   addons: [
     '@storybook/addon-svelte-csf',
     '@storybook/addon-links',
